0748a3f2dc2ae1030cd35f5873a1c4f1e773ee3c
AmitMT <amit.mathov@gmail.com>
2021-10-22 16:33:11 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App: React.FC = () => {
+	return (
+		<main className="min-h-screen flex justify-center items-center text-5xl font-bold">Hello!</main>
+	);
+};
+
+export default App;
